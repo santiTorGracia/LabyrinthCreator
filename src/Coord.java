d3cff85d01ddd10f5f9784b2b87f90ccae9ff9a1
Santi <sdetord@lasalle.cat>
2018-10-08 17:37:06 +0200

    Created Coord class to simplify handling two values (x, y).

---
+++ src/Coord.java
@@ -0,0 +1,40 @@
+import java.util.Objects;
+
+public class Coord {
+    private int x, y;
+
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Coord{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coord coord = (Coord) o;
+        return x == coord.x &&
+                y == coord.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
